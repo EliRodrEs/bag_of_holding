2c71ea81d09af90138ba4a6f22ef2321821306c7
EliRE <elisabetre.main@gmail.com>
2024-07-16 20:03:26 +0200

    Implement login form

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import user_login
 
 urlpatterns = [
-    path("", views.UserIndexView.as_view(), name="index")
+    path("", views.UserIndexView.as_view(), name="index"),
+    path("login/", user_login, name="login"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from django.contrib.auth import authenticate, login
 
 
 class UserIndexView(TemplateView):
     template_name = "user/index.html"
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/inventory')
+        else:
+            return render(request, 'user/index.html', {'error_message': 'Invalid login. Please try again.'})
+    else:
+        return render(request, 'user/index.html')
