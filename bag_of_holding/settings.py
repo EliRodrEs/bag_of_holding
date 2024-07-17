7ae9249d2a924b85c6aa80ab364b6c9d2558f3cf
EliRE <elisabetre.main@gmail.com>
2024-07-17 19:25:50 +0200

    Start main page implementation

--- bag_of_holding/settings.py
+++ bag_of_holding/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'inventory.apps.InventoryConfig',
     'user.apps.UserConfig',
-    'rosetta'
+    'rosetta',
+    'fontawesomefree'
 ]
 
 MIDDLEWARE = [
