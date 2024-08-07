6d0b203eeb8d22caaa0af3233ab146196603a9eb
EliRodrEs <elisabetre.main@gmail.com>
2024-08-07 22:40:15 +0200

    Implement better search method

--- inventory/static/inventory/js/inventory.js
+++ inventory/static/inventory/js/inventory.js
@@ -4,21 +4,3 @@ function toggleDetails(row) {
         nextRow.classList.toggle('hidden');
     }
 }
-
-const searchForItem = () => {
-    const inputValue = document.getElementById('hs-table-with-pagination-search').value;
-    const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    console.log(window.location.href)
-    fetch(window.location.href, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrfToken
-        },
-        body: JSON.stringify({ search_term: inputValue })
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('results-table').innerHTML = data.html;
-    });
-}
--- inventory/views.py
+++ inventory/views.py
@@ -15,13 +15,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        search_term = self.request.POST.get('search_term', '')
+        search_term = self.request.GET.get('search_term', '')
         if search_term:
             queryset = queryset.filter(Q(name__icontains=search_term) | Q(description__icontains=search_term))
         return queryset
-
-    def post(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
-        context = self.get_context_data()
-        html = render_to_string('inventory/item_list.html', context, request=request)
-        return JsonResponse({'html': html})
